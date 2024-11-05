ee84bf4912970cf0c77d13e587cade9b2a46ea8f
Ayaanleahmed <boqolhaji5@gmail.com>
2024-11-05 08:10:26 +0300

    first commit

--- utils/filehandler.js
+++ utils/filehandler.js
@@ -1,15 +1,21 @@
-const { json } = require('formidable');
-const fs = require ('fs');
-const path = require ('path');
-const filepath = './data/tasks.json'
-exports.writeTasksTofile =(tasks)=>{
-    fs.writeFileSync(filepath , JSON.stringify(tasks))
+const fs = require('fs');
+const path = require('path');
+
+const filePath = './data/tasks.json';
+
+exports.writeTasksToFile = (tasks) => {
+    if (Array.isArray(tasks) && tasks.length === 0) {
+        fs.writeFileSync(filePath, '[]');
+    } else {
+        fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+    }
 }
 
-exports.readTaskfromfile = () =>{
-    if(!fs.existsSync(filepath)){
-        writeTasksTofile([])
+exports.readTasksFromFile = () => {
+    if(!fs.existsSync(filePath)) {
+        this.writeTasksToFile([])
     }
-    const data = fs.readFileSync(filepath);
+
+    const data = fs.readFileSync(filePath);
     return JSON.parse(data)
 }
