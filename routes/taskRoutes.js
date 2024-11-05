f0d9c1331c7f447ee78b208b0da21e7486c749c9
Ayaanleahmed <boqolhaji5@gmail.com>
2024-11-05 08:08:34 +0300

    first commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,25 +1,20 @@
-const {getTasks} =require ("../controllers.taskControllers");
-const { json } = require("formidable")
+const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControllers");
 
-const taskRoutes = (req,res)=>{
-if(req.method ==='GET'){
-        getTasks(req,res)
-    }else if(req.method ==='POST'){
-        createTasks(req,res)
-    }else if(req.method ==='PATCH'){
-        updateTasks(req,res)
-}else if(req.method ==='DELETE'){
-    deleteTasks(req,res)
-
-}else {
-    res.writehead(404,'page not found',{ 'content-type':'application/json' })
-    req.end(JSON.stringify({
-        message: "unknown method required."
-    }))
-
+const taskRoutes = (req, res) => {
+    if(req.method === 'GET') {
+        getTasks(req, res);
+    } else if(req.method === 'POST') {
+        createTask(req, res)
+    } else if (req.method === 'PATCH') {
+        updateTask(req, res)
+    } else if(req.method === 'DELETE') {
+        deleteTask(req, res)
+    } else {
+        res.writeHead(404, 'Data Not Found', { 'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: "Unknown Method required."
+        }))
+    }
 }
 
-
-}
-
-module.exports = taskRoutes
+module.exports = taskRoutes;
